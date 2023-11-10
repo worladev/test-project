37b2db61a8695d33e7050a5c0b9c769e1358c063
worladev <confidenceworlasi@gmail.com>
2023-11-10 04:28:39 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def greet(name):
+    print(f'Hi, {name}')
+
+
+def age(user_age):
+    print(f'Your are {user_age} years old')
+
+
+if __name__ == '__main__':
+    greet('Confidence')
+    age(25)
+
+
